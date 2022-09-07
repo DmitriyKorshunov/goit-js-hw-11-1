2b23d9fa512cd368bb0fca5b7973dcae6e2f966e
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-09-07 13:55:51 +0300

    Запрос отправляет картинки получает

--- src/index.js
+++ src/index.js
@@ -1,25 +1,2 @@
-import fetchImages from "./js/fetch-images";
-import cardTemplate from "./card-template.hbs";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-import throttle from "lodash.throttle";
-import SimpleLightbox from "simplelightbox";
-import { refs } from "./js/refs-elements";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-console.log("Привет");
-//console.log(fetchImages("cat", 1));
-console.log(refs.searchForm);
-console.log(refs.gallery);
-console.log(refs.loadMoreBtn);
-console.log(refs.endCollectionText);
-
-function renderCardImage(arr) {
-  const markup = arr.map((item) => cardTemplate(item)).join("");
-  gallery.insertAdjacentHTML("beforeend", markup);
-}
-
-// let lightbox = new SimpleLightbox(".photo-card a", {
-//   captions: true,
-//   captionsdata: "alt",
-//   captionDelay: 250,
-// });
+import './sass/main.scss';
+import './js/search-images/index';
--- src/js/refs-elements.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  searchForm: document.querySelector(".search-form"),
-  gallery: document.querySelector(".gallery"),
-  loadMoreBtn: document.querySelector(".load-more"),
-  endCollectionText: document.querySelector(".end-collection-text"),
-};
